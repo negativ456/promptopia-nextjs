3733942aac3293dfaa6c402c2e82bb77827a8337
Daniel <negativ_2002@mail.ru>
2023-07-24 21:04:16 +0300

    update: rtk api

--- shared/api/rtkApi.ts
+++ shared/api/rtkApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const rtkApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.NEXTAUTH_URL}/api/`,
+    baseUrl: `${process.env.NEXTAUTH_URL}api/`,
   }),
   tagTypes: ["Prompt", "User"],
   endpoints: (builder) => ({}),
