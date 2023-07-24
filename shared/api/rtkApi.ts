c35943493e75149d214028835c43ae40ff1fbdb7
Daniel <negativ_2002@mail.ru>
2023-07-24 21:34:25 +0300

    update: rtk api

--- shared/api/rtkApi.ts
+++ shared/api/rtkApi.ts
@@ -5,7 +5,7 @@ const url = process.env.NEXTAUTH_URL ?? "http://localhost:3000";
 export const rtkApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${url}/api/`,
+    baseUrl: `https://promptopia-nextjs-ebon.vercel.app/api/`,
   }),
   tagTypes: ["Prompt", "User"],
   endpoints: (builder) => ({}),
